f0d64039ccdb101cc3246ef96ef3f89511d7b067
Marion Bobillo <marion.bobillo@gmail.com>
2024-07-24 16:23:03 +0200

    consulting flips

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get '/home' => "static_pages#home"
   get '/webdev' => "static_pages#webdev"
   get '/consulting' => "static_pages#consulting"
+  get '/contact' => "static_pages#contact"
   root 'static_pages#home'
 end
